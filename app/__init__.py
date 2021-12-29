bd3e4e3288837729d75adbef5432b1996b3cae11
DaleNaci <dtn22@njit.edu>
2021-12-29 01:46:33 -0500

    Added sqlalchemy stuff to handle basic database model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/site.db"
+db = SQLAlchemy(app)
+
+
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    date = db.Column(db.String(256), nullable=False)
+    teams = db.relationship("TeamCompLink", backref="competition", lazy=True)
+
+    def __repr__(self):
+        return f"Competition({self.id}, '{self.name}', '{self.date}')"
+
+
+class TeamCompLink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comp_id = db.Column(db.Integer, db.ForeignKey("competition.id"), nullable=False)
+    name = db.Column(db.String(6), nullable=False)
+
+    def __repr__(self):
+        return f"TeamCompLink({self.comp_id}, '{self.name}')"
+
+
 
 competitions = [
     {
